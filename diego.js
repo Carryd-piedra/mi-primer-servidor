b0af88c549e6fc35f11b7960eee33b87df44df9f
Carryd-piedra <diegocarrion972@gmail.com>
2025-10-03 09:28:19 -0500

    Rama actualizada

--- diego.js
+++ diego.js
@@ -1,5 +1,10 @@
 function Hello(){
 
-    var mensaje = "Hello Juan Developer"
+    var mensaje = "Hello Diego Developer"
     console.log(mensaje)
+
+    a = 5,b=10;
+    var suma = a + b;
+    console.log(suma)
 }
+
