de1a26afdb8bb2365c85f2b37ade764c4db91894
Daniel <daniel.urgiles.92@est.ucacue.edu.ec>
2025-10-13 23:23:26 -0500

    hola

--- index.js
+++ index.js
@@ -7,4 +7,14 @@ app.use(express.json());
 app.listen(process.env.PORT, () => {
     console.log("Servidor escuchando en el puerto " + process.env.PORT);
 });
+function Hello(){
+
+    var mensaje = "Hello Juan Developer cambio"
+    console.log(mensaje)
+}
+
+// funcion sumar dos numeros
+function sumar(a, b){
+    return a + b;
+}
 
